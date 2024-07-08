970ab052af997a2d0ddef5657f7740765d594ffa
amercamper <84207596+amercamper@users.noreply.github.com>
2024-07-09 02:40:49 +0300

    Update command.py

--- Youtube/command.py
+++ Youtube/command.py
@@ -1,14 +1,9 @@
-# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL | LISA-KOREA/YouTube-Video-Download-Bot
-
-# [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/YouTube-Video-Download-Bot
-
 from pyrogram import Client, filters
 import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
 from Youtube.config import Config
 from Youtube.script import Translation
 from Youtube.forcesub import handle_force_subscribe
-
 
 #########################
 
@@ -22,9 +17,6 @@ else:
     wish = "Good evening 🌝"
 
 
-
-
-########################🎊 Lisa | NT BOTS 🎊######################################################
 @Client.on_callback_query(filters.regex("cancel"))
 async def cancel(client, callback_query):
     await callback_query.message.delete()
@@ -33,46 +25,40 @@ async def cancel(client, callback_query):
 @Client.on_message(filters.private & filters.command("about"))
 async def about(client, message):
     if Config.CHANNEL:
-      fsub = await handle_force_subscribe(client, message)
-      if fsub == 400:
-        return
+        fsub = await handle_force_subscribe(client, message)
+        if fsub == 400:
+            return
     await message.reply_text(
         text=Translation.ABOUT_TXT,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton('⛔️ Close', callback_data='cancel')]
-        ]
-    ))
-
+            [
+                [InlineKeyboardButton('⛔️ Close', callback_data='cancel')]
+            ]
+        )
+    )
 
 # Start command handler
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
     if Config.CHANNEL:
-      fsub = await handle_force_subscribe(client, message)
-      if fsub == 400:
-        return
-    #user = message.from_user
+        fsub = await handle_force_subscribe(client, message)
+        if fsub == 400:
+            return
     await message.reply_text(
         text=Translation.START_TEXT.format(message.from_user.first_name, wish),
         reply_markup=InlineKeyboardMarkup(
-        [
             [
-                InlineKeyboardButton(' Update Channel', url='https://t.me/Jawfe'),
-            ],
-            [
-                InlineKeyboardButton(' Developer', url='https://t.me/Jawfe'),
-            ],
-            [
-                InlineKeyboardButton('⛔️ Close', callback_data='cancel')
+                [InlineKeyboardButton(' Update Channel', url='https://t.me/Jawfe')],
+                [InlineKeyboardButton(' Developer', url='https://t.me/Jawfe')],
+                [InlineKeyboardButton('⛔️ Close', callback_data='cancel')]
             ]
-        ]
-    ))
+        )
+    )
 
 # Help command handler
 @Client.on_message(filters.command("help"))
-def help(client, message):
+async def help(client, message):
     help_text = """
     Welcome to the YouTube Video Uploader Bot!
 
@@ -82,6 +68,4 @@ Enjoy using the bot!
 
    ©️ Channel : @jawfe
     """
-    message.reply_text(help_text)
-
-########################🎊 Lisa | NT BOTS 🎊######################################################
+    await message.reply_text(help_text)
