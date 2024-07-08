54a29051f3b4d811ac759cdd663266a30080b111
amercamper <84207596+amercamper@users.noreply.github.com>
2024-07-09 02:09:23 +0300

    Update script.py

--- Youtube/script.py
+++ Youtube/script.py
@@ -4,12 +4,6 @@ class Translation(object):
 
     ABOUT_TXT = """
 ╭───────────⍟
-├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
-├📢 **Framework** : [Pyrogram 2.0.106](https://docs.pyrogram.org/)
-├💮 **Language** : [Python 3.12.3](https://www.python.org)
-├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
-├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
-├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
-├🎓 **Developer** : [LISA 👑](https://t.me/LISA_FAN_LK)
+├🎓 **Developer** : [jawfe 👑](https://t.me/jawfe)
 ╰───────────────⍟
 """
